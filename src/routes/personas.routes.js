4a7dc1d2fbec8d6ba2ca4eb33c19d0793c177532
FelipeSanchezC <felipesanchez9600@hotmail.com>
2024-05-23 19:47:33 -0500

    Se finalizan las pruebas del proyecto

--- src/routes/personas.routes.js
+++ src/routes/personas.routes.js
@@ -3,15 +3,64 @@ import pool from '../database.js'
 
 const router = Router();
 
+router.get('/add', (req, res) =>{
+    res.render('personas/add')
+});
+
+router.post('/add' , async(req, res)=>{
+    try {
+        const {firstname, lastname, age} = req.body
+        const newPersona = {
+            firstname, lastname, age
+        }
+        await pool.query('INSERT INTO personas SET ?', [newPersona]);
+        res.redirect('/list');
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
 
 router.get('/list', async(req, res) => {
     try {
         const [result] = await pool.query('SELECT * FROM personas');
-        console.log(result);
         res.render('personas/list', {personas: result});
     } catch (error) {
         res.status(500).json({message: error.message});
     }
 });
+
+router.get('/delete/:id', async(req, res) =>{
+    try {
+        const {id} = req.params
+        await pool.query('DELETE FROM personas WHERE id = ?', [id]);
+        res.redirect('/list');
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+router.get('/edit/:id', async(req, res)=>{
+    try {
+        const {id} = req.params
+        const [persona] = await pool.query('SELECT * FROM personas WHERE id = ?', [id]);
+        const personaEdit = persona[0]
+        res.render('personas/edit', {persona: personaEdit});
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}); 
+
+router.post('/edit/:id', async(req, res) =>{
+    try {
+        const {id} = req.params
+        const {firstname, lastname, age} = req.body
+        const editPersona = {firstname, lastname, age}
+        await pool.query('UPDATE personas SET ? WHERE id = ?', [editPersona, id]);
+        res.redirect('/list')
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
 
 export default router;
